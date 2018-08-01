eb91a4a8a366f9df5b530a7c1c05a05a48d9b9d8
Jie Ping <pingjie811@gmail.com>
2018-08-01 15:14:11 -0500

    Update prepareSCRNADataSet.R

--- R/prepareSCRNADataSet.R
+++ R/prepareSCRNADataSet.R
@@ -36,41 +36,50 @@ prepareSCRNADataSet <- function(sampleTable, organism){
 ##' @importFrom Scran quickCluster computeSumFactors trendVar decomposeVar
 ##' @importFrom Rtsne Rtsne
 ##' @importFrom data.table data.table
+##' @importFrom Matrix.utils merge
 ##' @export preparePCATSNEData
 ##' @examples 
 ##' #sces <- prepareSCRNADataSet(sampleTable)
 ##' #sceall <- preparePCATSNEData(sces)
-preparePCATSNEData <- function(sces, ncomponents = 10, perplexity = 20) {
-	dt <- data.table(cbind(rownames(counts(sces[[1]]$sce)), counts(sces[[1]]$sce)), key = "V1")
-
-	conditions <- rep(names(sces)[1], dim(sces[[1]]$sce)[2])
-
-	if(length(sces) > 1){
-		for (i in 2:length(sces)) {
-			dt2 <- data.table(cbind(rownames(counts(sces[[i]]$sce)), counts(sces[[i]]$sce)), key = "V1")
-			dt <- merge(dt, dt2, by = "V1", all = T)
-			conditions <- c(conditions, rep(names(sces)[i], dim(sces[[i]]$sce)[2]))
-		}
-	}
-	dt[is.na(dt)] <- 0
-	ac <- apply(dt[, -1], 2, as.numeric)
-	rownames(ac) <- dt$V1
-
-	sceall <- SingleCellExperiment(list(counts = (as.matrix(ac))))
-	colData(sceall)$condition <- conditions
-
-	ave.counts <- calcAverage(sceall)
-	high.ave <- ave.counts >= 0.1
-	clusters <- quickCluster(sceall, subset.row = high.ave, method = "igraph")
-	sceall <- computeSumFactors(sceall, cluster = clusters, subset.row = high.ave, min.mean = 0, positive=TRUE)
-	sceall <- normalize(sceall)
-
-	sceall <- runPCA(sceall, ncomponents = ncomponents)
-
-	set.seed(100)
-
-	tsne_out <- Rtsne(reducedDim(sceall, "PCA"), initial_dims = ncol(reducedDim(sceall, "PCA")), pca = FALSE, perplexity = perplexity)
-	reducedDim(sceall, "TSNE") <- tsne_out$Y
-
-	return(sceall)
+reparePCATSNEData <- function(sces, ncomponents = 10, perplexity = 20) {
+  
+  allct <- data.table(cbind(rownames(counts(sces[[1]]$sce)), counts(sces[[1]]$sce)), key = "V1")
+  conditions <- rep(names(sces)[1], dim(sces[[1]]$sce)[2])
+  colnames(allct)[2:ncol(allct)] <- paste0(names(sces)[1], "cell", 1:dim(sces[[1]]$sce)[2])
+  
+  if(length(sces) > 1){
+    for (i in 2:length(sces)) {
+      
+      dt <- data.table(cbind(rownames(counts(sces[[i]]$sce)), counts(sces[[i]]$sce)), key = "V1")
+      
+      allct <- merge(allct, dt, by = "V1", all = T)
+      
+      conditions <- c(conditions, rep(names(sces)[i], dim(sces[[i]]$sce)[2]))
+      colnames(allct)[(ncol(allct) - dim(sces[[i]]$sce)[2] + 1):ncol(allct)] <- paste0(names(sces)[i], "cell", 1:dim(sces[[i]]$sce)[2])
+    }
+  }
+  allct[is.na(allct)] <- 0
+  
+  alldt <- apply(allct[, -1], 2, as.numeric)
+  rownames(alldt) <- allct$V1
+  
+  sceall <- SingleCellExperiment(list(counts = alldt))
+  
+  colData(sceall)$condition <- conditions
+  
+  ave.counts <- calcAverage(sceall)
+  high.ave <- ave.counts >= 0.1
+  clusters <- quickCluster(sceall, subset.row = high.ave, method = "igraph")
+  sceall <- computeSumFactors(sceall, cluster = clusters, subset.row = high.ave, min.mean = 0, positive=TRUE)
+  sceall <- normalize(sceall)
+  
+  sceall <- runPCA(sceall, ncomponents = ncomponents)
+  
+  set.seed(100)
+  
+  tsne_out <- Rtsne(reducedDim(sceall, "PCA"), initial_dims = ncol(reducedDim(sceall, "PCA")), pca = FALSE, perplexity = perplexity)
+  reducedDim(sceall, "TSNE") <- tsne_out$Y
+  
+  return(sceall)
 }
+
